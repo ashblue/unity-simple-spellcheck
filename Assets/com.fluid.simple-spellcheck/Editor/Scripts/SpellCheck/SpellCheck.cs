e3432c46bc76addffc7258d33e34b6530a30d643
Ash Blue <ash@blueashes.com>
2020-02-12 16:34:16 -0700

    feat: word error check now prints to an editor window

--- Assets/com.fluid.simple-spellcheck/Editor/Scripts/SpellCheck/SpellCheck.cs
+++ Assets/com.fluid.simple-spellcheck/Editor/Scripts/SpellCheck/SpellCheck.cs
@@ -1,7 +1,45 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
 
 namespace CleverCrow.Fluid.SimpleSpellcheck {
     public class SpellCheck {
+        private static SpellCheck _instance;
+        private SpellCheckInternal _internal;
+
+        public static SpellCheck Instance => _instance ?? (_instance = new SpellCheck());
+
+        private SpellCheck () {
+            TextAsset wordsTxt = null;
+            try {
+                wordsTxt = GetWordsTxt();
+            } catch (Exception e) {
+                Debug.LogError($"Could not find the dictionary text file. Aborting spell check setup {e}");
+                return;
+            }
+
+            var wordList = wordsTxt.text.Split(new []{"\r\n"}, StringSplitOptions.None);
+            var dictionary = new EnglishDictionary(new HashSet<string>(wordList));
+            _internal = new SpellCheckInternal(dictionary);
+        }
+
+        private static TextAsset GetWordsTxt () {
+            var guids = AssetDatabase.FindAssets("FluidWordsAlpha");
+            var wordsPath = AssetDatabase.GUIDToAssetPath(guids[0]);
+            return AssetDatabase.LoadAssetAtPath<TextAsset>(wordsPath);
+        }
+
+        public void Validate (string text) {
+            Debug.Log(text);
+            SpellCheckResults.ShowWindow(_internal.Validate(text));
+        }
+
+        [MenuItem("Spelling/Test")]
+        public static void TestSpellCheck () {
+            Instance.Validate("This is a block of tidddxx with some spalkling arrars. This isn't how your mom's dog normally wants you to spell.");
+        }
     }
 
     public class SpellCheckInternal {

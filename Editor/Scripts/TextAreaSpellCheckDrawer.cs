261852fedc475c394f7aa45a85b205ab9b95d311
Ash Blue <ash@blueashes.com>
2020-02-13 19:23:48 -0700

    fix: line breaks no longer crash spell check area

--- Editor/Scripts/TextAreaSpellCheckDrawer.cs
+++ Editor/Scripts/TextAreaSpellCheckDrawer.cs
@@ -24,13 +24,20 @@ namespace CleverCrow.Fluid.SimpleSpellcheck {
             }
         }
 
-        private static void PrintTextArea (Rect position, SerializedProperty property, int lines) {
+        private void PrintTextArea (Rect position, SerializedProperty property, int lines) {
             EditorGUI.BeginChangeCheck();
 
-            var textPosition = position;
-            textPosition.y += EditorGUIUtility.singleLineHeight;
-            textPosition.height = EditorGUIUtility.singleLineHeight * lines;
-            var text = GUI.TextArea(textPosition, property.stringValue, -1, GUI.skin.textArea);
+            var scrollPos = position;
+            scrollPos.y += EditorGUIUtility.singleLineHeight;
+            scrollPos.height = EditorGUIUtility.singleLineHeight * lines;
+
+            var textPos = new Rect(scrollPos) { x = 0, y = 0 };
+            textPos.width -= 10;
+            textPos.height = Mathf.Max(GUI.skin.textArea.CalcHeight(new GUIContent(property.stringValue), textPos.width), EditorGUIUtility.singleLineHeight * lines);
+
+            _scroll = GUI.BeginScrollView(scrollPos, _scroll, new Rect(0, 0, textPos.width, textPos.height), GUIStyle.none, GUI.skin.verticalScrollbar);
+            var text = GUI.TextArea(textPos, property.stringValue, -1, GUI.skin.textArea);
+            GUI.EndScrollView();
 
             if (EditorGUI.EndChangeCheck()) {
                 property.stringValue = text;
